e0211bfcbd57cb690c3fcc4a81316d44a4e46461
vgalli <valeria.galli@hest.ethz.ch>
2026-08-12 18:10:58 +0200

    Refactor result paths and update project structure

--- config.py
+++ config.py
@@ -6,7 +6,9 @@ ROOT_DIR = Path(__file__).resolve().parent
 DATA_DIR = ROOT_DIR / "dataset"
 REPORTS_FP = DATA_DIR / "TCGA_Reports.csv"
 RESULTS_DIR = ROOT_DIR / "results"
-RESULTS_DIRECT_API_DIR = RESULTS_DIR / "directAPI"
-RESUTLS_LLAMA_DIR = RESULTS_DIR / "llamaindex"
 
-MODEL_NAME = "openai/gpt-oss-120b"
+DIRECT_API_DIR = RESULTS_DIR / "direct_api"
+DIRECT_API_EXTRACTIONS_DIR = DIRECT_API_DIR / "extractions"
+DIRECT_API_VALIDATION_DIR = DIRECT_API_DIR / "validation"
+
+DIRECT_API_MODEL_NAME = "openai/gpt-oss-120b"
